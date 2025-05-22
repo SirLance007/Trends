ff7a6b63c6760d1f896122dd2060ad9c9f122c43
Prankur Sharma <prankursharma40@gmail.com>
2025-05-22 15:37:02 +0530

    commit

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const Product = require("./models/Product")
-const User = require("./models/User");
+const Product = require("./models/Product");
+const User = require("../models/user");
 const products = require("./data/products");
 const Cart = require("./models/Cart");
-
 
 dotenv.config();
 
@@ -14,39 +13,36 @@ mongoose.connect(process.env.MONGO_URI);
 // Function to seed data
 
 const seedData = async () => {
-    try {
-        // Clear existing data
-        await Product.deleteMany();
-        await User.deleteMany();
-        await Cart.deleteMany();
+  try {
+    // Clear existing data
+    await Product.deleteMany();
+    await User.deleteMany();
+    await Cart.deleteMany();
 
-        // Create a default admin User
+    // Create a default admin User
 
-        const createdUser = await User.create({
-            name : "Admin User",
-            email : "admin@example.com",
-            password : "123456",
-            role : "admin",
-        });
+    const createdUser = await User.create({
+      name: "Admin User",
+      email: "admin@example.com",
+      password: "123456",
+      role: "admin",
+    });
 
-        // Assign the default user ID to each product 
-        const userID = createdUser.id;
-        const sampleProduct = products.map((product) => {
-            return {...product ,user :  userID}
-        });
+    // Assign the default user ID to each product
+    const userID = createdUser.id;
+    const sampleProduct = products.map((product) => {
+      return { ...product, user: userID };
+    });
 
-        // Inset the products into the database
-        await Product.insertMany(sampleProduct);
+    // Inset the products into the database
+    await Product.insertMany(sampleProduct);
 
-        console.log("Product data seeded successfully!");
-        process.exit();
-    }
-    catch (error){
-        console.log("Error seeding in the data : " , error);
-        process.exit(1);
-    }
+    console.log("Product data seeded successfully!");
+    process.exit();
+  } catch (error) {
+    console.log("Error seeding in the data : ", error);
+    process.exit(1);
+  }
 };
-
-
 
 seedData();
